6323a3140aac903f5f8fb67c00ae964d396a4922
GlebShaucou <shevcov.gleb@gmail.com>
2016-11-21 11:41:03 +0300

    More solutions

--- js-assignments/task/03-date-tasks.js
+++ js-assignments/task/03-date-tasks.js
@@ -22,7 +22,8 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+    // return new Date(value);
+    throw new Error('Not implemented');
 }
 
 /**
@@ -37,7 +38,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+    return new Date(value);
 }
 
 
@@ -56,7 +57,16 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+    var year = date.getFullYear();
+    if(!(year % 400)) {
+        return true;
+    } else if(!(year % 100)) {
+        return false;
+    } else if(!(year % 4)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 
@@ -76,7 +86,11 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+    var start = new Date(startDate);
+    var end = new Date(endDate);
+    var diff = new Date(end.valueOf() - start.valueOf());
+
+    return diff.toISOString().slice(-13, -1);
 }
 
 
@@ -94,6 +108,22 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
+    // var hours = date.getHours();
+    // var min = date.getMinutes();
+
+    // if(hours > 12) {
+    //     hours = hours - 12;
+    // } 
+    // if(hours == 0) {
+    //     hours = 12;
+    // }
+
+    // var angle = Math.abs(0.5 * (60 * hours - 11 * min));
+    // if (angle > 180) {
+    //     angle = 360 - angle;
+    // } 
+    // console.log((angle * Math.PI) / 180);
+    // return (angle * Math.PI) / 180;
     throw new Error('Not implemented');
 }
 
